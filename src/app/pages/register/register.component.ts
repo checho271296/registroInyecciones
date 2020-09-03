90c8a283b5a26350db915736742b1f3c27a104d7
Sergio <sfonseca@estudiantec.cr>
2020-09-03 10:39:34 -0600

    add features

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
 import { InfoService } from '../../services/info.service';
 import { Injection } from '../../models/inyection.model';
 import { RouterLink, Router } from '@angular/router';
-
+declare var jsPDF: any;
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,7 +11,7 @@ import { RouterLink, Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  
+  flagInfo = false;
   injections : Array<any> = [];
   inyection = new Injection();
   constructor(public infoService:InfoService ,private router: Router) { }
@@ -19,10 +20,17 @@ export class RegisterComponent implements OnInit {
     this.loadInfo();
   }
 
+  sortFunction(a:any,b: any){  
+    var dateA = new Date(a.payload.doc.data().fecha).getTime();
+    var dateB = new Date(b.payload.doc.data().fecha).getTime();
+    return dateA > dateB ? 1 : -1;  
+  }; 
+
    loadInfo(){
     this.infoService.getInjections()
     .subscribe(result => {
       this.injections = result;
+      this.injections.sort(this.sortFunction)
     })  
    }
 
@@ -49,6 +57,16 @@ export class RegisterComponent implements OnInit {
           console.log(err);
         }
       )   
+  }
+
+  convert(){
+    let doc = new jsPDF();
+    this.injections.forEach(function(info, i){
+    doc.text(20, 10 + (i * 10), 
+        "First Name: " + info.a.payload.doc.data().fecha +
+        "Last Name: " + info.a.payload.doc.data().nivelAzucar);
+    });
+    doc.save('Test.pdf');
   }
 
 }
